763a3a0b5b400a673fdf86e7aadb1d86a0109c76
ns loni <nsloni@gmail.com>
2025-04-26 09:37:19 +0530

    add

--- playground.py
+++ playground.py
@@ -1,48 +1,46 @@
-import openai
 from phi.agent import Agent
-import phi.api
-from phi.model.openai import OpenAIChat
+from phi.model.groq import Groq
 from phi.tools.yfinance import YFinanceTools
 from phi.tools.duckduckgo import DuckDuckGo
 from dotenv import load_dotenv
-from phi.model.groq import Groq
-
 import os
-import phi
 from phi.playground import Playground, serve_playground_app
+
 # Load environment variables from .env file
 load_dotenv()
+groq_api_key = os.getenv("GROQ_API_KEY")  # Load Groq API key
 
-phi.api=os.getenv("PHI_API_KEY")
-
-## web search agent
-web_search_agent=Agent(
+# Web Search Agent
+web_search_agent = Agent(
     name="Web Search Agent",
     role="Search the web for the information",
-    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview", api_key=groq_api_key),
     tools=[DuckDuckGo()],
-    instructions=["Alway include sources"],
-    show_tools_calls=True,
+    instructions=["Always include sources"],
+    show_tool_calls=True,
     markdown=True,
-
 )
 
-## Financial agent
-finance_agent=Agent(
+# Finance Agent
+finance_agent = Agent(
     name="Finance AI Agent",
-    model=Groq(id="llama3-groq-70b-8192-tool-use-preview"),
+    model=Groq(id="llama3-groq-70b-8192-tool-use-preview", api_key=groq_api_key),
     tools=[
-        YFinanceTools(stock_price=True, analyst_recommendations=True, stock_fundamentals=True,
-                      company_news=True),
+        YFinanceTools(
+            stock_price=True,
+            analyst_recommendations=True,
+            stock_fundamentals=True,
+            company_news=True
+        )
     ],
     instructions=["Use tables to display the data"],
     show_tool_calls=True,
     markdown=True,
-
 )
 
-app=Playground(agents=[finance_agent,web_search_agent]).get_app()
+# Create Playground App
+app = Playground(agents=[finance_agent, web_search_agent]).get_app()
 
-if __name__=="__main__":
-    serve_playground_app("playground:app",reload=True)
-
+# Run Playground App
+if __name__ == "__main__":
+    serve_playground_app("playground:app", reload=True)
